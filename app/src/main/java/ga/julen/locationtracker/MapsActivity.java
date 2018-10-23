75d78841e9528a3fc14dd7a6a3225b44997c3ef7
julenb <21701719@live.uem.es>
2018-10-23 19:04:45 +0200

    Mostrar anteriores

--- app/src/main/java/ga/julen/locationtracker/MapsActivity.java
+++ app/src/main/java/ga/julen/locationtracker/MapsActivity.java
@@ -1,12 +1,15 @@
 package ga.julen.locationtracker;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,6 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private SQLiteOpenHelper sqLite;
 
     private ArrayList<Location> locations;
 
@@ -29,7 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         Intent intent = getIntent();
-        locations = intent.getParcelableArrayListExtra("locations");
+        locations = ubicacionesPrevias();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,26 +41,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    private ArrayList<Location> ubicacionesPrevias() {
+        ArrayList<Location> ubicaciones = new ArrayList<>();
+        sqLite = new SQLiteOpenHelper(this, "ubicaciones", null, 1) {
+            @Override
+            public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+            }
+        };
+        Cursor cursor = sqLite.getWritableDatabase().rawQuery("SELECT * FROM ubicaciones WHERE ID = (SELECT MAX(ID) FROM ubicaciones);", null);
+        while (cursor.moveToNext()) {
+            Location location = new Location("");
+            location.setLatitude(cursor.getFloat(1));
+            location.setLongitude(cursor.getFloat(2));
+            ubicaciones.add(location);
+        }
+        Cursor cursor2 = sqLite.getWritableDatabase().rawQuery("SELECT * FROM ubicaciones;", null);
+        while (cursor2.moveToNext()) {
+            Log.d("ubicacion", cursor2.getInt(0) + " " + cursor2.getFloat(1) + cursor2.getFloat(2));
+        }
+        return ubicaciones;
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         PolylineOptions line =
                 new PolylineOptions();
-        LatLng lastLocation=new LatLng(-34, 151);
+        LatLng lastLocation = new LatLng(-34, 151);
+        boolean firstLocation = true;
         for (Location location : locations) {
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
             line.add(latLng);
-            mMap.addMarker(new MarkerOptions().position(latLng));
-            lastLocation=latLng;
+            if (firstLocation) {
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Primera ubicación."));
+            }
+            firstLocation = false;
+            lastLocation = latLng;
         }
         line.width(5).color(Color.RED);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation));
+        googleMap.addMarker(new MarkerOptions().position(lastLocation).title("Última ubicación."));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, 17.0f));
         googleMap.addPolyline(line);
 
     }
