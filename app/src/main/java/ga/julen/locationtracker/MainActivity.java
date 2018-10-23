490ec5d4ba0c05cc2f4ad0250ad9002a1c5e5ee5
julenb <21701719@live.uem.es>
2018-10-23 16:45:40 +0200

    Lista de localizaciones

--- app/src/main/java/ga/julen/locationtracker/MainActivity.java
+++ app/src/main/java/ga/julen/locationtracker/MainActivity.java
@@ -1,6 +1,10 @@
 package ga.julen.locationtracker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkLocationPermission();
         btnComenzar = findViewById(R.id.btn_comenzar);
         btnComenzar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,4 +28,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
 }
