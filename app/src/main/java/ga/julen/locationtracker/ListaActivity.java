cb196fbef704f8aed160d4bdd19c6c3b39c5e27d
julenb <21701719@live.uem.es>
2018-11-23 20:38:07 +0100

    Mostrar ListView de Sesiones

--- app/src/main/java/ga/julen/locationtracker/ListaActivity.java
+++ app/src/main/java/ga/julen/locationtracker/ListaActivity.java
@@ -1,13 +1,58 @@
 package ga.julen.locationtracker;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.LinkedHashMap;
 
 public class ListaActivity extends AppCompatActivity {
+
+    private SessionsAdapter sessionsAdapter;
+    private ListView listView;
+    private SQLiteOpenHelper sqLite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+        listView = findViewById(R.id.listView);
+        popularListView();
     }
+
+    private void popularListView() {
+        sqLite = new SQLiteOpenHelper(this, "ubicaciones", null, 1) {
+            @Override
+            public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+            }
+        };
+        LinkedHashMap<Integer, String> spinnerArray = new LinkedHashMap<>();
+        Cursor cursor2 = sqLite.getWritableDatabase().rawQuery("SELECT ID, FECHA_HORA FROM sesiones ORDER BY ID DESC;", null);
+        while (cursor2.moveToNext()) {
+            spinnerArray.put(cursor2.getInt(0), cursor2.getString(1));
+        }
+        sessionsAdapter = new SessionsAdapter(spinnerArray, this);
+        listView.setAdapter(sessionsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("idSesion", sessionsAdapter.getKey(position));
+                startActivity(intent);
+            }
+        });
+    }
+
 }
